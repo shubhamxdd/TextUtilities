13c8f7586bdf8e87c4daa4262a7472d9fdbabd32
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-01-07 02:01:35 +0530

    Revert "changes for gh pages deployemnt (remove router)"
    This reverts commit 2a0fb248d79a683c5fa62ea5b967715d5374ab53.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Navbar(props) {
   return (
@@ -10,12 +10,9 @@ export default function Navbar(props) {
     >
       {/* data-bs-theme="dark" for dark navbar*/}
       <div className="container-fluid">
-        {/* <Link className="navbar-brand" to="/">
+        <Link className="navbar-brand" to="/">
           {props.title}
-        </Link> */}
-        <a className="navbar-brand" href="/">
-          {props.title}
-        </a>
+        </Link>
         <button
           className="navbar-toggler"
           type="button"
@@ -30,20 +27,14 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              {/* <Link className="nav-link active" aria-current="page" to="/">
+              <Link className="nav-link active" aria-current="page" to="/">
                 Home
-              </Link> */}
-              <a className="nav-link active" aria-current="page" href="/">
-                Home
-              </a>
+              </Link>
             </li>
             <li className="nav-item">
-              {/* <Link className="nav-link" to="About">
+              <Link className="nav-link" to="About">
                 {props.aboutText}
-              </Link> */}
-              {/* <a className="nav-link" href="About">
-                {props.aboutText}
-              </a> */}
+              </Link>
             </li>
             {/* <li className="nav-item">
               <Link className="nav-link" to="/">
